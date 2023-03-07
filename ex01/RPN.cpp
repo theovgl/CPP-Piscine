9e2880ee1e46b06dbadcfa2db430e05c43afefc3
tvogel <tvogel@student.42.fr>
2023-03-07 20:22:11 +0100

    Squashed 'CPP-Module-09/' content from commit 3642e09
    git-subtree-dir: CPP-Module-09
    git-subtree-split: 3642e09aeffdcd29b9e09f81c74f2ba2cf8aadb3

---
+++ ex01/RPN.cpp
@@ -0,0 +1,90 @@
+#include "includes/RPN.hpp"
+
+RPN::RPN(char *input)
+{
+	std::string inputString = input;
+
+	for (size_t i = 0; i < inputString.size(); i++) {
+		if (!_isCharValid(input[i])) {
+			throw BadCharacter();
+		}
+	}
+}
+
+RPN::~RPN()
+{
+}
+
+int RPN::evaluate(char *input)
+{
+	for (size_t i = 0; input[i] != '\0'; i++) {
+		if (isdigit(input[i])) {
+			_stack.push(input[i] - '0');
+		} else if (_isOperator(input[i])) {
+			if (_stack.size() < 2)
+				throw std::exception();
+			int result;
+			int second_operand = _stack.top();
+			_stack.pop();
+
+			int first_operand = _stack.top();
+			_stack.pop();
+
+			switch (input[i]) {
+				case '+':
+					result = _add(first_operand, second_operand); break;
+				case '-':
+					result = _substract(first_operand, second_operand); break;
+				case '/':
+					if ( second_operand == 0 )
+						throw DivisionByZero();
+					result = _divide(first_operand, second_operand); break;
+				case '*':
+					result = _multiple(first_operand, second_operand); break;
+			}
+			_stack.push(result);
+		}
+	}
+	return _stack.top();
+}
+
+int RPN::_add(int first_operand, int second_operand)
+{
+	return (first_operand + second_operand);
+}
+
+int RPN::_substract(int first_operand, int second_operand)
+{
+	return (first_operand - second_operand);
+}
+
+int RPN::_multiple(int first_operand, int second_operand)
+{
+	return (first_operand * second_operand);
+}
+
+int RPN::_divide(int first_operand, int second_operand)
+{
+	return (first_operand * second_operand);
+}
+
+bool RPN::_isOperator(char c) const
+{
+	if(c == '+'
+	|| c == '-'
+	|| c == '/'
+	|| c == '*')
+	{
+		return true;
+	}
+	return false;
+}
+
+bool RPN::_isCharValid(char c) const
+{
+	if (isdigit(c) || _isOperator(c) || c == ' ')
+	{
+		return true;
+	}
+	return false;
+}
